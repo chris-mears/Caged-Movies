d7cd279d75db4ae4ad1608bfe2580e97146691c7
SadEvilPenguin <mears.chris@gmail.com>
2017-11-10 17:20:53 -0500

    built out reviews controller

--- app/controllers/api/reviews_controller.rb
+++ app/controllers/api/reviews_controller.rb
@@ -1,2 +1,34 @@
 class Api::ReviewsController < ApplicationController
+
+  def index
+    @reviews = Review.includes(:movie).all
+    render json: @reviews, include: [:movie]
+  end
+
+  def show
+    @review = Review.includes(:movie).find(params[:id])
+    render json: @review, include: [:movie]
+  end
+
+  def create
+    @review = Review.create!(review_params)
+    render json: @review, include: [:movie]
+  end
+
+  def update
+    @review = Review.find(params[:id])
+    @review.update_attributes(review_params)
+    render json: @review, include: [:movie]
+  end
+
+  def destroy
+    @review = Review.delete(params[:id])
+    render json: { msg: "Delete Successful" }
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:title, :body, :likes, :genre, :user_id, :movie_id)
+  end
 end

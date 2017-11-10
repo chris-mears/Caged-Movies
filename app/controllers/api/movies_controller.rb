7e36bc3486923e6ee4b0bfc3b779504d713376cd
SadEvilPenguin <mears.chris@gmail.com>
2017-11-10 16:08:12 -0500

    update movie controller

--- app/controllers/api/movies_controller.rb
+++ app/controllers/api/movies_controller.rb
@@ -1,2 +1,13 @@
 class Api::MoviesController < ApplicationController
+
+  #Index method for Movies
+  def index
+    @movies = Movie.all
+    render json: @movies
+  end
+
+  def show
+    @movie = Movie.find_by_id(params[:id])
+    render json: @movie
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
 
   namespace :api do
-    resources :movies, only: [:create, :update, :show, :all]
-    resources :reviews, only: [:create, :destroy, :update, :show, :all]
+    resources :movies, only: [:create, :update, :show, :index]
+    resources :reviews, only: [:create, :destroy, :update, :show, :index]
     resources :favorite_movies, only: [:create, :destroy, :update]
     resources :movie_comment, only: [:create, :destroy, :update]
     resources :review_likes, only: [:create, :destroy]

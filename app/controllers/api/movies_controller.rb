7307800f446b419dfc22551d21fc471cc762ff4f
SadEvilPenguin <mears.chris@gmail.com>
2017-11-10 16:39:19 -0500

    built out movie controller

--- app/controllers/api/movies_controller.rb
+++ app/controllers/api/movies_controller.rb
@@ -10,4 +10,21 @@ class Api::MoviesController < ApplicationController
     @movie = Movie.find_by_id(params[:id])
     render json: @movie
   end
+
+  def update
+    @movie = Movie.find_by_id(params[:id])
+    @movie.update_attributes(movie_params)
+    render json: @movie
+  end
+
+  def create
+    @movie = Movie.create!(movie_params)
+    render json: @movie
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:title, :plot, :poster, :rating, :likes, :api_id, :imdb_id, :tag_line, :genre)
+  end
 end

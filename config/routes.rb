c9e0d38a1057f528def4f36f0b29cbffc73bcbfc
SadEvilPenguin <mears.chris@gmail.com>
2017-11-10 15:39:55 -0500

    updated seeds and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+    resources :movies, only: [:create, :update, :show, :all]
+    resources :reviews, only: [:create, :destroy, :update, :show, :all]
+    resources :favorite_movies, only: [:create, :destroy, :update]
+    resources :movie_comment, only: [:create, :destroy, :update]
+    resources :review_likes, only: [:create, :destroy]
+    resources :review_comments, only: [:create, :destroy, :update]
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,56 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+Movie.destroy_all
+
+bob_loblaw = User.create!(
+  email: 'bob@bob.com',
+  password: 'password',
+  password_confirmation: 'password'
+)
+
+chris_mears = User.create!(
+  email: 'chris@chris.com',
+  password: 'password',
+  password_confirmation: 'password'
+)
+
+the_room = Movie.create!(
+  title: "The Room",
+  plot: "Johnny is a successful banker with great respect for and dedication to the people in his life, especially his future wife Lisa. The happy-go-lucky guy sees his world fall apart when his friends begin to betray him one-by-one.",
+  poster: "https://image.tmdb.org/t/p/original/aUC39cFC2KO8CJ0EV0ijIJRr3PT.jpg",
+  rating: 6.366738,
+  likes: 1,
+  api_id: 17473,
+  imdb_id: "tt0368226",
+  tag_line: "Can you ever really trust anyone?",
+  genre: "Drama"
+)
+
+street_fighter = Movie.create!(
+  title: "Street Fighter",
+  plot: "Col. Guile and various other martial arts heroes fight against the tyranny of Dictator M. Bison and his cohorts.",
+  poster: "https://image.tmdb.org/t/p/original/1Q5YdK91oJBXJcbCuZhKXeY4vP1.jpg",
+  rating: 10.861827,
+  likes: 1,
+  api_id: 11667,
+  imdb_id: "tt0111301",
+  tag_line: "The fight to save the world is on!",
+  genre: "Action"
+)
+
+room_review = Review.create!(
+  title: "Oh, Hi Mark",
+  body: "What puts 'The Room' in a class of its own is its overabundant idiocy. While you're still gasping at one of the film's moronic lines or vagrant plot knots, another one rises up to smack you in the head.",
+  likes: 2,
+  genre: "Drama",
+  user_id: chris_mears.id,
+  movie_id: the_room.id
+)
+
+fighter_review = Review.create!(
+  title: "The muscles from brussels as an American Colonel?",
+  body: "A dreary, overstuffed hodgepodge of poorly edited martial arts sequences and often unintelligible dialogue.",
+  likes: 1,
+  genre: "Action",
+  user_id: bob_loblaw.id,
+  movie_id: street_fighter.id
+)

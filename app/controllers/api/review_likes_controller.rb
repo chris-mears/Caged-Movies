87a065b03cc51d6457bd1a1c8507381d5274c3f1
SadEvilPenguin <mears.chris@gmail.com>
2017-11-15 11:44:57 -0500

    built out review likes controller

--- app/controllers/api/review_likes_controller.rb
+++ app/controllers/api/review_likes_controller.rb
@@ -1,2 +1,21 @@
 class Api::ReviewLikesController < ApplicationController
+  def create
+    @user = current_user
+    @review_like = @user.review_likes.build(review_like_params)
+    if @user.save
+      render json: @review_like
+    end
+  end
+
+  def destroy
+    @user = current_user
+    @review_like = ReviewLike.find(params[:id]).delete
+    render json: { msg: "Delete Successful" }
+  end
+
+  private
+
+  def review_like_params
+    params.require(:review_like).permit(:review_id)
+  end
 end
